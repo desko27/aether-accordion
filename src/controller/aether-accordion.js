ba0d99ac74dae4d4dcca756c202c6ad54af53b99
desko27 <desko27@gmail.com>
2018-11-10 17:03:21 +0100

    Create base implementation and tests for AetherAccordion

--- src/controller/aether-accordion.js
+++ src/controller/aether-accordion.js
@@ -1,3 +1,22 @@
-// export default class AetherAccordionController {
-//
-// }
+import {
+  throwArgumentMissingError,
+  throwArgumentTypeError
+} from "../utils/error";
+import AetherItemController from "./aether-item";
+
+export default class AetherAccordionController {
+  constructor({ entries } = {}) {
+    // check for missing arguments
+    if (entries === undefined) throwArgumentMissingError("entries");
+
+    // validate incoming arguments
+    if (!Array.isArray(entries))
+      throwArgumentTypeError("entries", entries, "array");
+
+    // parse entries' data into actual item controllers
+    this.entries = entries.map(entry => new AetherItemController(entry));
+
+    // settle other properties into the instance
+    this.activeId = null;
+  }
+}
---
+++ src/controller/tests/aether-accordion.test.js
@@ -0,0 +1,49 @@
+import AetherAccordionController from "../aether-accordion";
+import AetherItemController from "../aether-item";
+
+const validArgs = {
+  entries: [
+    {
+      id: 0,
+      title: "Section #1",
+      description: "Section #1 contents..."
+    },
+    {
+      id: 1,
+      title: "Section #2",
+      description: "Section #2 contents..."
+    },
+    {
+      id: 2,
+      title: "Section #3",
+      description: "Section #3 contents..."
+    }
+  ]
+};
+
+const defaultProperties = {
+  activeId: null
+};
+
+describe("AetherAccordionController", () => {
+  const validAetherAccordion = new AetherAccordionController(validArgs);
+  // let aetherAccordion;
+
+  describe("has a constructor method that", () => {
+    describe("sets the following properties when it receives valid arguments:", () => {
+      it("entries", () => {
+        expect(validAetherAccordion.entries).to.exist;
+        expect(validAetherAccordion.entries)
+          .to.be.an("array")
+          .that.deep.equals(
+            validArgs.entries.map(entry => new AetherItemController(entry))
+          );
+      });
+      it("activeId", () => {
+        expect(validAetherAccordion.activeId).to.equal(
+          defaultProperties.activeId
+        );
+      });
+    });
+  });
+});
