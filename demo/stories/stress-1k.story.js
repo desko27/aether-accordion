b57de3b0f026750e39fcbd27e8b1968debe0be7a
desko27 <desko27@gmail.com>
2018-11-23 22:04:51 +0100

    Add margin for p children at dd tags

--- demo/stories/stress-1k.story.js
+++ demo/stories/stress-1k.story.js
@@ -8,7 +8,7 @@ export default () => ({
       entries: [...new Array(1000).keys()].map(i => ({
         id: i,
         title: `Title #${i + 1}`,
-        description: `Description #${i + 1}`
+        description: `<p>Description #${i + 1}</p>`
       }))
     });
   }
