de39b9cdb4e3a44a3ae454cf76e7c53de4179ad2
desko27 <desko27@gmail.com>
2018-11-23 23:10:25 +0100

    Better messages for nesting story

--- demo/stories/nesting.story.js
+++ demo/stories/nesting.story.js
@@ -3,43 +3,35 @@ import initAetherAccordion from "../../src";
 export default () => ({
   html: `
     <dl class="level-1">
-      <dt>Section 1</dt>
-      <dd>
-        <p>Section 1 Content...</p>
-      </dd>
-      <dt>Section 2</dt>
+      <dt>You don't wanna go down... do you?</dt>
       <dd>
         <dl class="level-2">
-          <dt>Section 1</dt>
+          <dt>Nothing to be seen here</dt>
           <dd>
-            <p>Section 1 Content...</p>
+            <p>I told you!</p>
           </dd>
-          <dt>Section 2</dt>
+          <dt>Wanna go even deeper?</dt>
           <dd>
             <dl class="level-3">
-              <dt>Section 1</dt>
+              <dt>Oh yeah</dt>
               <dd>
-                <p>Section 1 Content...</p>
+                <p>Lorem ipsum</p>
               </dd>
-              <dt>Section 2</dt>
+              <dt>I just got tired</dt>
               <dd>
-                <p>Section 2 Content...</p>
+                <p>Lorem ipsum</p>
               </dd>
-              <dt>Section 3</dt>
+              <dt>Let's leave it here</dt>
               <dd>
-                <p>Section 3 Content...</p>
+                <p>Lorem ipsum</p>
               </dd>
             </dl>
           </dd>
-          <dt>Section 3</dt>
-          <dd>
-            <p>Section 3 Content...</p>
-          </dd>
         </dl>
       </dd>
-      <dt>Section 3</dt>
+      <dt>Keep calm</dt>
       <dd>
-        <p>Section 3 Content...</p>
+        <p>...and stay at the first level.</p>
       </dd>
     </dl>
   `,
